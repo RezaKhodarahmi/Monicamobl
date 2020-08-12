3a7da00f28800077fc308fc3944457c08d6feb0d
reza khodarahmi <mrkhodarahmii@gmail.com>
2020-08-12 20:51:32 +0430

    Updates

---
+++ precache-manifest.c8729c0a16299c136cf91c2b8d1a366e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e01fc2a9807508a4857d99a1f19775",
+    "url": "/%7BMonicamobl%7D/index.html"
+  },
+  {
+    "revision": "af1ea8eeb9912cfd1ccb",
+    "url": "/%7BMonicamobl%7D/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6496dd8620df27196d67",
+    "url": "/%7BMonicamobl%7D/static/css/main.f6fdbfaa.chunk.css"
+  },
+  {
+    "revision": "af1ea8eeb9912cfd1ccb",
+    "url": "/%7BMonicamobl%7D/static/js/2.b64e29a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/%7BMonicamobl%7D/static/js/2.b64e29a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6496dd8620df27196d67",
+    "url": "/%7BMonicamobl%7D/static/js/main.8d239ee8.chunk.js"
+  },
+  {
+    "revision": "4768dab67f9376e2b018",
+    "url": "/%7BMonicamobl%7D/static/js/runtime-main.edee002c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7BMonicamobl%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
